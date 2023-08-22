600c4d013f5100ad5dfb7b27d4debc59e8180662
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2023-08-23 02:46:57 +0530

    release date fixed

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,7 +1,8 @@
 import InfiniteScroll from "react-infinite-scroll-component";
 import noResult from "../../assets/no-results.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchDataFromApi } from "../../utils/api";
 
 const SearchResult = () => {
   const [data, setData] = useState(null);
@@ -9,7 +10,40 @@ const SearchResult = () => {
   const [loading, setLoading] = useState(false);
   const { query } = useParams();
 
-  return <div className="searchResultsPage">SearchResult</div>;
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((prev) => prev + 1);
+        setLoading(false);
+      }
+    );
+  };
+
+  const fetchNextPageData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        if (data?.results) {
+          // only add the results part, old results + new results 
+          setData({
+            ...data,
+            results: [...data.results, ...res.results],
+          });
+        } else {
+          setData(res);
+        }
+        setLoading(false);
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+  }, [query]);
+
+  return <div className="searchResultsPage"></div>;
 };
 
 export default SearchResult;
