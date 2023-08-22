95701b80af89c82b9d6372dffe148a1257c461c5
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2023-08-23 04:06:41 +0530

    initial commit

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -3,6 +3,10 @@ import noResult from "../../assets/no-results.png";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchDataFromApi } from "../../utils/api";
+import Spinner from "../../components/spinner/Spinner";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import "./style.scss";
+import MovieCard from "../../components/movieCard/MovieCard";
 
 const SearchResult = () => {
   const [data, setData] = useState(null);
@@ -22,11 +26,10 @@ const SearchResult = () => {
   };
 
   const fetchNextPageData = () => {
-    setLoading(true);
     fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
       (res) => {
         if (data?.results) {
-          // only add the results part, old results + new results 
+          // only add the results part, old results + new results
           setData({
             ...data,
             results: [...data.results, ...res.results],
@@ -34,16 +37,55 @@ const SearchResult = () => {
         } else {
           setData(res);
         }
-        setLoading(false);
+        setPageNum((prev) => prev + 1);
       }
     );
   };
 
   useEffect(() => {
+    setPageNum(1);
     fetchInitialData();
   }, [query]);
 
-  return <div className="searchResultsPage"></div>;
+  return (
+    <div className="searchResultsPage">
+      {loading && <Spinner initial={true} />}
+      {!loading && (
+        <ContentWrapper>
+          {data?.results?.length > 0 ? (
+            <>
+              <div className="pageTitle">
+                {`Search ${
+                  data.total_results > 1 ? "results" : "result"
+                } of '${query}'`}
+              </div>
+              <InfiniteScroll
+                className="content"
+                dataLength={data?.results?.length || []}
+                next={fetchNextPageData}
+                hasMore={pageNum <= data.total_pages}
+                loader={<Spinner />}
+              >
+                {data.results.map((item, index) => {
+                  if (item.media_type === "person") return;
+                  return (
+                    <MovieCard
+                      key={index}
+                      data={item}
+                      mediaType={item?.media_type}
+                      fromSearch={true}
+                    />
+                  );
+                })}
+              </InfiniteScroll>
+            </>
+          ) : (
+            <span className="resultNotFound">Sorry, Results not found</span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
+  );
 };
 
 export default SearchResult;
